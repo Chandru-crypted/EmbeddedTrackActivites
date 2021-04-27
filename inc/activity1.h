c87624efee797579daa0a3d7246b64c07cb93692
Chandru-crypted <chander.sekaran12@gmail.com>
2021-04-27 20:37:37 +0530

    just add activity.h and activity.c

---
+++ inc/activity1.h
@@ -0,0 +1,14 @@
+/**
+ * @file activity1.h
+ * @author Chandrasekaran
+ * @brief 
+ * @version 0.1
+ * @date 2021-04-27
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef __ACTIVITY1_H__
+#define __ACTIVITY1_H__
+
+#endif
